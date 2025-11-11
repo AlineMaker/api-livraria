82296373037dd752adacb75e74c3a4f22a1c84e3
ALINE ALVES DE SOUZA <25132561@CFP127ALUNO.LOCAL>
2025-11-11 11:28:24 -0300

    started controller_livro

---
+++ controller/livro/controller_livro.js
@@ -0,0 +1,38 @@
+/************************************************************************************************************************************************************************
+ * OBjetivo: Controller responsável pela manipulação de dados, validação e regra de negócio para o crud de livros.
+ * Data: 11/11/2025
+ * Autor: Aline Alves de Souza
+ * Versão: 1.0 
+ ************************************************************************************************************************************************************************/
+//6° fazer import do arquivo DAO para manipular dados no BD
+const livroDAO = require(`../../model/DAO/livro.js`)
+
+
+
+
+// 5° fazer as funcoes de crud para o controller - inserção, atualização, exclusão e busca de dados
+// Retorna uma lista de livros
+
+const listarLivros = async function () {
+    
+}
+
+//Retorna um livro filtrando pelo ID
+const buscarLivroId = async function (id) {
+    
+}
+
+//Insere um livro
+const inserirLivro = async function (livro) {
+    
+}
+
+// Atualiza um livro filtrando pelo ID
+const atualizarLivro = async function (livro, id) {
+    
+}
+
+// Exclui um livro filtrando pelo ID
+const excluirLivro = async function (id) {
+    
+}
---
+++ model/DAO/livro.js
@@ -0,0 +1,62 @@
+/************************************************************************************************************************************************************************
+ * OBjetivo: Model responsável pela realização do CRUD de livros, esse arquivo fará acesso a base de dados MySQL
+ * data 11/11/2025
+ * Autor: Aline Alves de Souza
+ * Versão: 1.0 
+ ************************************************************************************************************************************************************************/
+//2° Incluir uma biblioteca do Node.js que faça integração com a base de Dados(Prisma)
+
+//  Import Prisma 
+const { PrismaClient} = require('@prisma/client')
+
+//Cria um novo objeto para manipular as transações com Banco de Dados
+const prisma = new PrismaClient()
+
+//1° Camada de Manipulação e acesso aos dados
+
+
+// Pelo menos 4 funções - na verdade 6 
+
+//função que pega todos os dados - Retorna uma lista de livros
+//função anônima
+//async espera o banco de dados dar o retorno
+
+
+
+//3° Chama o sql para buscar informações da sua tabela no banco de dados do mysql 
+const getSelectAllBooks = async function () {
+    let sql = `select * from tbl_livro`
+
+    let result = await prisma.$queryRawUnsafe(sql)
+
+    if(result)
+        return result;
+    else
+    return false;
+}
+
+//4° instala prisma
+//npm install prisma --save
+//npm install @prisma/client --save
+//npx prisma init
+
+// Retorna um livro pelo ID
+const getSelectBookById = async function (id) {
+    
+}
+
+//Insere um novo livro 
+
+const setInsertBook = async function (livro) {
+    
+}
+//Atualiza um livro existente
+
+const setUpdateBook = async function (livro) {
+    
+}
+
+//Exclui um livro pelo ID
+const setDeleteBokk = async function (livro) {
+    
+}
